aef983329f9dc104a4426da8f08b19761f4580bf
aldi-milano <milano.aldi@gmail.com>
2022-07-30 14:18:27 +0700

    revised setTimeOut time

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -10,7 +10,7 @@ function SearchItem({ title, price, img }) {
   useEffect(() => {
     setTimeout(() => {
       heightRef.current.addEventListener('load', getSpans());
-    }, 100);
+    }, 300);
   }, []);
 
   function getSpans() {

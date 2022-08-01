6c67aab0145795f4103c523a8cc862563de465c0
aldi-milano <milano.aldi@gmail.com>
2022-08-01 17:18:41 +0700

    adding and convert currency to IDR

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,5 +1,6 @@
 import React, { useRef, useEffect, useState } from 'react';
 import Wishlist from '../navigation/icon/Wishlist';
+import { IoCartOutline } from 'react-icons/io5';
 import './card.scss';
 
 function SearchItem({ title, price, img }) {
@@ -31,10 +32,20 @@ function SearchItem({ title, price, img }) {
       </div>
       <div className='card__desc'>
         <div className='container'>
-          <p className='card__desc-price'>{price}</p>
+          <p className='card__desc-price'>
+            {Math.round(price * 14871).toLocaleString('id-ID', {
+              style: 'currency',
+              currency: 'IDR',
+              minimumFractionDigits: 0,
+            })}
+          </p>
           <Wishlist />
         </div>
         <p className='card__desc-title'>{title}</p>
+        <button className='button--cart'>
+          <IoCartOutline className='icon--cart' />
+          <p>Add to cart</p>
+        </button>
       </div>
     </div>
   );
